1511a327d96aa1caaed6f10d87d440bd84b9055a
rgoogle <rs1766728441@gmail.com>
2023-10-09 14:22:15 +0800

    intro example

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -1,3 +1,11 @@
+/*
+ * @Author: rgoogle rs1766728441@gmail.com
+ * @Date: 2023-10-09 10:44:44
+ * @LastEditors: rgoogle rs1766728441@gmail.com
+ * @LastEditTime: 2023-10-09 11:04:38
+ * @FilePath: \RustDance\rust-rustlings-2023-autumn-Rgoogle\exercises\intro\intro2.rs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // intro2.rs
 //
 // Make the code print a greeting to the world.
@@ -8,5 +16,5 @@
 // I AM NOT DONE
 
 fn main() {
-    println!("Hello {}!");
+    println!("Hello {}!","world");
 }
