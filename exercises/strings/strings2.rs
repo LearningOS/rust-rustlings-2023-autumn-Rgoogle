d96e7edaaa6c31112223a77285ebe21364af2b46
rgoogle <rs1766728441@gmail.com>
2023-10-13 08:44:13 +0800

    string finish .

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -7,9 +7,12 @@
 
 // I AM NOT DONE
 
+use std::ops::Deref;
+
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+
+    if is_a_color_word(word.as_str()) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
